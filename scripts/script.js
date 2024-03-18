e3956aeb88b45e9e6425536e48bbfdbe8d974af1
Mana1422 <manasesvillalobos80@gmail.com>
2024-03-17 22:25:34 -0700

    Modify the login and sign up page, added a pop up window to modify the user's information, and a pop up window to add a new asset.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,29 @@
 function sayHello() {
 }
 //sayHello();
+
+// modal for the profile popup window
+const modal = document.querySelector('#profile-pop-up');
+const openModal = document.querySelector('#open-popup-profile');
+const closeModal = document.querySelector('#close-popup-profile');
+
+openModal.addEventListener('click', () =>{
+    modal.showModal();
+})
+
+closeModal.addEventListener('click', function() {
+    modal.close();
+})
+
+// modal to add a stock to your portfolio
+const modal1 = document.querySelector('#add-stock-popup');
+const openModalAddStock = document.querySelector('#add-stock');
+const closeModalAddStock = document.querySelector('#close-addStock-popup');
+
+openModalAddStock.addEventListener('click', function() {
+    modal1.showModal();
+})
+
+closeModalAddStock.addEventListener('click', function() {
+    modal1.close();
+})
